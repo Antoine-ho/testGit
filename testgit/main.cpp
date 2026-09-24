a5d5fbe7173c49095e03c32bebce127941e2af3d
Antoine-ho <hottekiet@gmail.com>
2026-09-24 15:02:39 +0200

    hello

--- testgit/main.cpp
+++ testgit/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::cout << "bonjour" << std::endl;
+	std::cout << "hellooooooooooooooooooooooooo" << std::endl;
 	std::cout << "ca va" << std::endl;
 	std::cout << "oui" << std::endl;
 	return 0;
